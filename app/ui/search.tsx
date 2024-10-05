9be7877a137236e528ab99c53df31742957e1d7a
wu529778790 <529778790@qq.com>
2024-10-05 23:02:53 +0800

    优化发票页面分页功能，修复搜索重置页码问题

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,6 +12,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
     if (term) {
       params.set("query", term);
     } else {
@@ -19,6 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
